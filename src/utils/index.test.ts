46ba4a426b04d847e28e069f27189e65f66b347c
leonard <leonardllzznn@gmail.com>
2023-02-16 00:12:47 +0800

    feat: url hashing function with tests

--- src/utils/index.test.ts
+++ src/utils/index.test.ts
@@ -1,8 +1,35 @@
 import { hashUrl } from ".";
 
-describe("hash url util function", () => {
-  it("generate hash string", () => {
+describe("hash url util fn", () => {
+  it("output string should be hashed", () => {
+    const url = "https://google.com";
+    const hash = hashUrl(url);
+
+    expect(hash).not.toBe(url);
+  });
+
+  it("output string should have length of 10", () => {
     const hash = hashUrl("https://google.com");
-    expect(hash).toBe("https://google.com");
+
+    expect(hash).toHaveLength(10);
+  });
+
+  it("two output string should be same when input string and secret are same", () => {
+    const url = "https://google.com";
+    const secret = "secret";
+    const hash1 = hashUrl(url, secret);
+    const hash2 = hashUrl(url, secret);
+
+    expect(hash1).toBe(hash2);
+  });
+
+  it("two output string should be different when secrets are different", () => {
+    const url = "https://google.com";
+    const secret1 = "secret1";
+    const secret2 = "secret2";
+    const hash1 = hashUrl(url, secret1);
+    const hash2 = hashUrl(url, secret2);
+
+    expect(hash1).not.toBe(hash2);
   });
 });
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
-export const hashUrl = (url: string) => {
-  return url;
-};
+import { createHmac } from "crypto";
+
+export const hashUrl = (url: string, secret = "HASH_PASSWORD") =>
+  createHmac("sha512", secret).update(url).digest("base64").slice(0, 10);
