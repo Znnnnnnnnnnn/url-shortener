8a42d0a3a6ec1c0f334fa05d4a87cb52ddbb8442
leonard <leonardllzznn@gmail.com>
2023-02-16 01:16:35 +0800

    feat: redirect function and test

--- e2e/redirect.spec.ts
+++ e2e/redirect.spec.ts
@@ -1,6 +1,12 @@
 import { test, expect } from "@playwright/test";
 
-test("test home page e2e", async ({ page }) => {
-  await page.goto("/");
-  await expect(page).toHaveTitle("Create Next App");
+test("test redirect on visiting shorten url", async ({ page }) => {
+  // simulating accessing the shorten url
+  const res = await page.goto("/some-hashed-url");
+
+  // get the origin url before get redirected
+  const origin = await res?.request().redirectedFrom()?.response();
+
+  // assert status code 307 which indicate a temporary redirect
+  await expect(origin?.status()).toBe(307);
 });
